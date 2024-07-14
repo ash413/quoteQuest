212088b06a485ae5840d799e8a7fc5ddf6447b86
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-07-14 23:47:11 +0530

    js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
     let btn = document.querySelector('#new-qoute');
     let quote = document.querySelector('.quote');
+    let person = document.querySelector('.person');
 
     async function fetchData(){
         try {
-            let response = await fetch("https://api.quotable.io");
-            let data = response.json();
+            /*fetch("https://type.fit/api/quotes")
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            });*/
+            let response = await fetch("https://type.fit/api/quotes");
+            let data = await response.json();
             console.log(data);
         } catch (error) {
             console.log("Error is:" + error)
@@ -13,6 +21,6 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     console.log(fetchData());
     btn.addEventListener("click", function(){
-        fetchData();
+        
     })
 })
